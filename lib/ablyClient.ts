16bba15d2c6fe439c69463a94bc09cdc8a7ae433
IamYordan94 <ver.iamyo94@gmail.com>
2025-09-15 17:42:26 +0200

    Update ablyClient.ts

--- lib/ablyClient.ts
+++ lib/ablyClient.ts
@@ -1,10 +1,11 @@
-import * as Ably from 'ably';
+import Ably from 'ably/promises';
 
-export async function createRealtimeClient(): Promise<Ably.Realtime}> {
+export async function createRealtimeClient() {
   const res = await fetch('/api/ably/token', { cache: 'no-store' });
   if (!res.ok) throw new Error('Failed to get Ably token');
   const { tokenRequest } = await res.json();
-  const client = new Ably.Realtime.Promise({ authUrl: undefined, authCallback: (_tokenParams, callback) => callback(null, tokenRequest) });
-  return client as unknown as Ably.Realtime;
+  const client = new Ably.Realtime.Promise({ authCallback: (_tokenParams, callback) => callback(null, tokenRequest) });
+  return client;
 }
+
 
